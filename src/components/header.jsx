7b00862dde1b13d7badaeefb36bb7c28e6612f3e
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-04-12 17:22:15 +0200

    remove and checkout page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./header.css";
 
 function Header() {
+  const { totalProductsInCart } = useSelector((state) => state.cart);
+  console.log("TotaleCart", totalProductsInCart);
   return (
     <div className="header-wrapper">
       <div className="header inner">
-        <a className="logo" href="/">
+        <Link to="/" className="logo">
           Intra
-        </a>
+        </Link>
         <nav className="nav-wrapper">
           <Link to="/products">Store</Link>
           <Link to="/cart" className="cart-wrapper">
             <img src="../cart.svg" alt="cart" />
-            <p className="cart-items">10</p>
+            <p className="cart-items">{totalProductsInCart}</p>
           </Link>
         </nav>
       </div>
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 // import products from "./modules/products";
 import productsSlice from "./modules/products";
 import loaderSlice from "./modules/loaderSlice";
+import cartSlice from "./modules/cartSlice";
 
 const reducer = combineReducers({
   //i will list my modules
   products: productsSlice,
   loader: loaderSlice,
+  cart: cartSlice,
 });
 
 const index = configureStore({
